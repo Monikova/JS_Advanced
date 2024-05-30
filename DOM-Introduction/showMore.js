9645d82ff6459d0e6ade03649ae768061c2abd85
Monikova <monikova@abv.bg>
2024-05-30 23:15:34 +0300

    Create showMore.js

---
+++ DOM-Introduction/showMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    const textToDisplayElem = document.getElementById('text');
+    const textToHideElem = document.getElementById('more');
+
+    textToHideElem.style.display = 'none';
+    textToDisplayElem.style.display = 'inline';
+}
