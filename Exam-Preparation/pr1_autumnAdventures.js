007e5f93c72be37edccb864394606aa2de5cae4f
Monika Kiskinova <monikova@abv.bg>
2024-06-18 20:58:23 +0300

    Update pr1_autumnAdventures.js
    Continue functionality added

--- Exam-Preparation/pr1_autumnAdventures.js
+++ Exam-Preparation/pr1_autumnAdventures.js
@@ -10,38 +10,42 @@ function solve() {
     const addBtnElem = document.getElementById("add-btn");
 
     const ulLastCheckElem = document.getElementById("check-list");
+    const ulUpcomingElem = document.getElementById("upcoming-list");
 
     addBtnElem.addEventListener("click", addOnClick);
+
 
     function addOnClick(event) {
         const time = timeElem.value;
         const date = dateElem.value;
         const place = placeElem.value;
-        const eventinfo = eventInfoElem.value;
+        const eventInfo = eventInfoElem.value;
         const contacts = contactsElem.value;
 
-        if (!time || !date || !place || !eventinfo || !contacts) {
+        if (!time || !date || !place || !eventInfo || !contacts) {
             return;
         }
 
-        const pDateAndTimeElem = document.createElement("p");
-        pDateAndTimeElem.textContent = `Begins: ${date} at: ${time}`;
-        const pPlaceElem = document.createElement("p");
-        pPlaceElem.textContent = `In: ${place}`;
-        const pEventElem = document.createElement("p");
-        pEventElem.textContent = `Event: ${eventinfo}`;
-        const pContactElem = document.createElement("p");
-        pContactElem.textContent = `Contact: ${contacts}`;
-        const articleElem = document.createElement("article");
-        
-        articleElem.appendChild(pDateAndTimeElem);
-        articleElem.appendChild(pPlaceElem);
-        articleElem.appendChild(pEventElem);
-        articleElem.appendChild(pContactElem);
+        // const pDateAndTimeElem = document.createElement("p");
+        // pDateAndTimeElem.textContent = `Begins: ${date} at: ${time}`;
+        // const pPlaceElem = document.createElement("p");
+        // pPlaceElem.textContent = `In: ${place}`;
+        // const pEventElem = document.createElement("p");
+        // pEventElem.textContent = `Event: ${eventInfo}`;
+        // const pContactElem = document.createElement("p");
+        // pContactElem.textContent = `Contact: ${contacts}`;
 
-        const liElem = document.createElement("li");
-        liElem.classList.add("event-content");
-        liElem.appendChild(articleElem);
+        // const articleElem = document.createElement("article");
+        // articleElem.appendChild(pDateAndTimeElem);
+        // articleElem.appendChild(pPlaceElem);
+        // articleElem.appendChild(pEventElem);
+        // articleElem.appendChild(pContactElem);
+
+        // const liElem = document.createElement("li");
+        // liElem.classList.add("event-content");
+        // liElem.appendChild(articleElem);
+
+        const liElem = createArticleAndLiElements(time, date, place, eventInfo, contacts);
 
         const editBtn = createBtn("edit-btn", "Edit", onEdit);
         const continueBtn = createBtn("continue-btn", "Continue", onContinue);
@@ -54,15 +58,42 @@ function solve() {
         clearInputFields(timeElem, dateElem, placeElem, eventInfoElem, contactsElem);
         toggleBtn(addBtnElem);
     }
+
+
+    function createArticleAndLiElements(time, date, place, eventInfo, contacts) {
+        const pDateAndTimeElem = document.createElement("p");
+        pDateAndTimeElem.textContent = `Begins: ${date} at: ${time}`;
+        const pPlaceElem = document.createElement("p");
+        pPlaceElem.textContent = `In: ${place}`;
+        const pEventElem = document.createElement("p");
+        pEventElem.textContent = `Event: ${eventInfo}`;
+        const pContactElem = document.createElement("p");
+        pContactElem.textContent = `Contact: ${contacts}`;
+
+        const articleElem = document.createElement("article");
+        articleElem.appendChild(pDateAndTimeElem);
+        articleElem.appendChild(pPlaceElem);
+        articleElem.appendChild(pEventElem);
+        articleElem.appendChild(pContactElem);
+
+        const liElem = document.createElement("li");
+        liElem.classList.add("event-content");
+        liElem.appendChild(articleElem);
+
+        return liElem;
+    }
     
+
     function clearInputFields(...fields) {
         fields.forEach(f => f.value = "");
     }
+
 
     function toggleBtn(btn) {
         btn.disabled = !btn.disabled;
     }
     
+
     function createBtn (classes, text, handler) {
         const btn = document.createElement("button");
         btn.classList.add(classes);
@@ -71,14 +102,15 @@ function solve() {
         return btn;
     }
 
+
     function onEdit(event) {
         const parentElem = event.currentTarget.parentElement;
         const pElements = parentElem.querySelectorAll("p");
-        const [dateAndTime, place, eventArr, email] = Array.from(pElements).map(el => el.textContent.split(": "));
+        const [dateAndTime, placeArr, eventArr, email] = Array.from(pElements).map(el => el.textContent.split(": "));
 
         timeElem.value = dateAndTime[2];
         dateElem.value = dateAndTime[1].split(" ")[0];
-        placeElem.value = place[1];
+        placeElem.value = placeArr[1];
         eventInfoElem.value = eventArr[1];
         contactsElem.value = email[1];
 
@@ -87,6 +119,31 @@ function solve() {
         toggleBtn(addBtnElem);
     }
 
-    function onContinue() {}
+
+    function onContinue(event) {
+        const parentElem = event.currentTarget.parentElement;
+        const pElements = parentElem.querySelectorAll("p");
+        const [dateAndTime, placeArr, eventArr, email] = Array.from(pElements).map(el => el.textContent.split(": "));
+
+        const time = dateAndTime[2];
+        const date = dateAndTime[1].split(" ")[0];
+        const place = placeArr[1];
+        const eventInfo = eventArr[1];
+        const contacts = email[1];
+        
+        const liElem = createArticleAndLiElements(time, date, place, eventInfo, contacts);
+
+        const moveBtn = createBtn("finished-btn", "Move to Finished", onMove);
+        liElem.appendChild(moveBtn);
+
+        ulUpcomingElem.appendChild(liElem);
+
+        event.currentTarget.parentElement.remove();
+
+        toggleBtn(addBtnElem);
+    }
+
+
+    function onMove() {}
 
 }
